2c094ce4b5eb0edec2fa62c3770d91a4dca35f10
Do Dinh Van <87236474+Okaiii-2021@users.noreply.github.com>
2021-12-10 23:08:28 +0700

    Add files via upload

---
+++ controller/regulator/draw_1dof_sys.m
@@ -0,0 +1,15 @@
+clc;
+close all;
+
+
+% plot([0;3],[0;0] ,':yo','linewidth' ,2)
+% 
+
+
+for i = 1:6:length(out.the)
+    plot([0;cos(out.the(i))], [0;sin(out.the(i))], ':bo', 'linewidth', 2);
+    xlim([-2 2])
+    ylim([-2 2])
+    grid on;
+    pause(0.1);
+end
